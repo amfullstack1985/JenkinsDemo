bfce26a08f81b5237bb48af32cf0051eabae7561
ashwini <amfullstack22@gmail.com>
2022-08-09 10:17:53 +0530

    Added all files

---
+++ src/com/jenkins/demo/DemoJenkin.java
@@ -0,0 +1,10 @@
+package com.jenkins.demo;
+
+public class DemoJenkin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("Performing Jenkins Demo");
+	}
+
+}
